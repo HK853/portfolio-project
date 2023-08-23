b0a9fa0ccb4e6f8cc9b0b74ce5c8d5b71439881f
HK853 <harshninawe41@gmail.com>
2023-08-23 19:47:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// //   theme  //
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
+
+// nav menu code
+
+const btn=document.querySelector(".navbtn")
+const navheader = document.querySelector(".header")
+const p = document.querySelector(".nav-links")
+
+const togglenavbar = () =>{
+ 
+(navheader.classList.toggle("active"));
+
+//     p.style.display="none";
+//    }else{
+//     p.style.display="block";
+//    }
+};
+btn.addEventListener('click' ,() => togglenavbar());
+
+// animation
+
+const sr = ScrollReveal({
+  distance: '100px',
+  duration: 5000,
+  delay: 200,
+  reset: true
+});
+
+sr.reveal('.intro', {delay:'50',origin:'bottom'});
+sr.reveal('.pic', {delay:'50', origin:'left'});
+
